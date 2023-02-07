80962d23ab0788e27069cec021db2467894fe5e0
vitekkor <viktor.korotkikh@gmail.com>
2023-02-07 16:02:48 +0300

    add protobuf plugin

--- testOracle/build.gradle.kts
+++ testOracle/build.gradle.kts
@@ -1,12 +1,57 @@
+import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    id("com.google.protobuf") version "0.9.2"
 }
 
+val grpcKotlinVersion = "1.3.0"
+val grpcVersion = "1.47.0"
+val protobufVersion = "3.21.2"
+
 dependencies {
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+    implementation("org.apache.commons:commons-exec:1.3")
+
     testImplementation(kotlin("test"))
 }
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("../protobuf")
+        }
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
