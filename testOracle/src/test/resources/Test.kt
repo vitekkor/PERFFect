c6d2fb921b749e3885a5255bd3b058ba9be65b1b
vitekkor <viktor.korotkikh@gmail.com>
2023-03-02 23:04:56 +0300

    #5: add KotlinJVMCompilerTest

---
+++ testOracle/src/test/resources/Test.kt
@@ -0,0 +1,296 @@
+package src.crusting
+
+open abstract class Plantains(var slaves: Function1<Int, Int>, var retries: Function1<Long, Double>) {
+    open fun rippled(sleighs: Long, cameroons: Long): Long {
+        val peru: Benelux = ((TODO() as Benelux)).volta()
+        var sups: Benelux = peru.volta()
+        peru.manila = peru.manila
+        return sups.volta().exorcizes
+    }
+}
+
+open abstract class Benelux(open var exorcizes: Long, open var manila: Function0<Long>) {
+    open abstract fun volta(): Benelux
+}
+
+class Signaled(val parches: Bowsprits, val vandals: Learns<out Byte>) :
+    Plantains({ tibia: Int -> -40 }, fun(adds: Long): Double {
+        val pried: Bowsprits = Bowsprits()
+        val caroler: Benelux = ((TODO() as Anime<String>)).monday
+        val torpedoed: Number = caroler.volta().exorcizes
+        mathis(45)
+        return pried.similarly(torpedoed, Gimmicks('9'))
+    }) {
+    override fun rippled(sleighs: Long, cameroons: Long): Long =
+        65.toLong()
+}
+
+class Bowsprits : Plantains({ reeved: Int -> 52 }, fun(loosening: Long): Double {
+    val blenches: Boolean = true
+    val rapiers: Acacia = Acacia()
+    val movement: Acacia = (if (blenches)
+        Acacia()
+    else
+        rapiers)
+
+    fun potables(classed: Function0<Plantains> = { -> rapiers }): Function1<Int, Int> {
+        val hanker: Function1<Int, Int> = { ail: Int -> -6 }
+        val adopt: Byte = -7
+        val bound: Float = 39.301f
+        rulings(adopt, bound)
+        return hanker
+    }
+    Acacia().slaves = potables()
+    return movement.mutated(50.215)
+}) {
+    fun similarly(hypo: Number, cameos: Gimmicks): Double {
+        var subtitle: Double = 66.675
+        return subtitle
+    }
+}
+
+class Acacia : Plantains(fun(puck: Int): Int {
+    var comings: Int = 32
+    var adornment: Char = 'D'
+    var homeboy: Gimmicks = Gimmicks(adornment)
+    homeboy.vigilante()
+    return comings
+}, fun(reassured: Long): Double {
+    val funky: Double = 51.959
+    return funky
+}) {
+    fun mutated(tangshan: Double): Double =
+        -39.325
+
+    override fun rippled(sleighs: Long, cameroons: Long): Long {
+        var ismail: Long = 5
+        return ismail
+    }
+}
+
+class Gimmicks(val triads: Char) : Plantains(fun(barracuda: Int): Int {
+    var winded: Int = -92
+    return winded
+}, { deathless: Long -> -57.141 }) {
+    fun vigilante(): Unit {
+        (TODO() as Acacia)
+    }
+
+    override fun rippled(sleighs: Long, cameroons: Long): Long {
+        var waldo: Long = 71
+        fun myles(loosed: Boolean): Unit {
+            val strife: Boolean = true
+            strife
+        }
+
+        val raffles: Boolean = true
+        myles(raffles)
+        return waldo
+    }
+}
+
+fun rulings(spartans: Any, slurp: Float): Unit {
+    (TODO() as Signaled)
+}
+
+open abstract class Anime<Z : String>(open val monday: Benelux, override var manila: Function0<Long>) :
+    Benelux(14, { -> 52.toLong() })
+
+fun mathis(hallowing: Int): Unit {
+    fun azazel(
+        pervading: Anime<in String> = (TODO() as Anime<String>),
+        party: Gimmicks = Gimmicks('c')
+    ): Array<out Byte> {
+        val rimmed: Array<out Byte> = ((TODO() as Berated)).marksmen
+        var gamin: Array<out Byte> = rimmed
+        return gamin
+    }
+
+    val gyrating: Char = 'E'
+    val furies: Gimmicks = Gimmicks(gyrating)
+    var bounce: Roster = Roster(fun(): Bowsprits {
+        val manages: Bowsprits = (TODO() as Bowsprits)
+        val backs: Responded = Responded()
+        backs.companies(58)
+        return manages
+    }, -51.224f)
+    ((TODO() as Learns<Any>)).settles<Any>(bounce.nineveh)
+    azazel()
+}
+
+open abstract class Berated(open var marksmen: Array<out Byte>) : Plantains(fun(kickoff: Int): Int {
+    var geller: Int = 39
+    val changing: Boxwood<Char, Short> = (TODO() as Boxwood<Char, Short>)
+    changing.admirers(99.780)
+    return geller
+}, fun(canberra: Long): Double {
+    var restfully: Double = -39.859
+    val dossiers: Double = -54.938
+    restfully = dossiers
+    return restfully
+}) {
+    open override fun rippled(sleighs: Long, cameroons: Long): Long {
+        var jocasta: Long = -7
+        scarcely()
+        return jocasta
+    }
+
+    open abstract fun gauzier(
+        clans: Function3<Signaled, Char, Int, Double> = fun(angelina: Signaled, silvery: Char, ballrooms: Int): Double {
+            val striped: Double = 21.970
+            val elongates: Parkinson = (TODO() as Parkinson)
+            var versify: Parkinson = elongates
+            ((TODO() as Parkinson)).malone(marksmen)
+            return striped
+        }
+    ): Function3<Signaled, Char, Int, Double>
+}
+
+interface Boxwood<O : Any, N : Short> {
+    open abstract fun admirers(maturing: Double): Unit
+
+    open abstract fun ciders(sorrel: Double): Function3<Int, Signaled, Acacia, Any>
+}
+
+fun scarcely(barmaid: Array<out Signaled> = emptyArray<Signaled>()): Unit {
+    var cosigned: Anime<String> = (TODO() as Anime<String>)
+    var megaphone: Long = -2
+    cosigned.exorcizes = megaphone
+    Gimmicks('a')
+}
+
+open abstract class Parkinson(open override val monday: Benelux, var anoints: Function1<Any, Bowsprits>) :
+    Anime<String>((TODO() as Benelux), { -> (-94).toLong() }) {
+    open abstract fun malone(harks: Any, hawing: Parkinson = (TODO() as Parkinson)): Unit
+
+    open abstract fun sharped(
+        chinos: Function0<Plantains> = fun(): Plantains {
+            var rapacity: Gimmicks = Gimmicks(TODO())
+            return rapacity
+        }, loretta: Int = -40
+    ): Short
+}
+
+open abstract class Learns<R : Any>(open val reelected: Float) :
+    Plantains({ fondles: Int -> 8 }, { sonata: Long -> 12.183 }) {
+    open fun <F_T : R> settles(snuffle: F_T): Unit {
+        val patty: Short = 15
+        fun blanks(slopped: F_T): Unit {
+            (TODO() as Parkinson)
+        }
+
+        var prissiest: F_T = (TODO() as F_T)
+        blanks(prissiest)
+        patty
+    }
+
+    override fun rippled(sleighs: Long, cameroons: Long): Long {
+        var met: Long = -19
+        return met
+    }
+}
+
+open class Roster(open var nineveh: Function0<out Bowsprits>, override val reelected: Float) : Learns<Char>(-9.533f) {
+    override fun <F_T : Char> settles(snuffle: F_T): Unit {
+        val demurely: Function1<F_T, F_T> = fun(fleshed: F_T): F_T {
+            val clavicle: F_T = (TODO() as F_T)
+            val bandaged: Growers = (TODO() as Growers)
+            bandaged.wriest(Bowsprits())
+            return clavicle
+        }
+        demurely
+    }
+
+    fun <F_B : Any> ishtar(): F_B {
+        val slices: F_B = (TODO() as F_B)
+        nineveh = TODO()
+        return slices
+    }
+}
+
+interface Growers : Boxwood<Parkinson, Short> {
+    open abstract fun wriest(unwell: Bowsprits): Unit
+}
+
+class Responded : Anime<String>((TODO() as Benelux), fun(): Long {
+    val thrash: Long = -78
+    return thrash
+}) {
+    fun companies(dejecting: Short): Unit {
+        var mesquites: Anime<String> = Responded()
+        mascaraed(mesquites)
+        (TODO() as Signaled)
+    }
+
+    open override fun volta(): Benelux {
+        var ribs: Benelux = Responded()
+        return ribs
+    }
+}
+
+fun mascaraed(panting: Benelux): Unit {
+    var accessory: Short = 51
+    accessory = (-81).toShort()
+    accessory
+}
+
+interface Bridal<W : String, P : Any> : Boxwood<P, Short> {
+    open abstract fun dispenses(): W
+
+    open abstract fun yates(trainee: P, chamois: Benelux): Byte
+}
+
+fun starless(scampered: Learns<in Parkinson> = (TODO() as Learns<Parkinson>), caned: Int = -36): Int =
+    caned
+
+class Ina<U : Any, Q : Responded>(var moved: Berated, val lineages: Q) : Boxwood<Q, Short> {
+    open override fun admirers(maturing: Double): Unit {
+        fun sophocles(untested: U = (TODO() as U), muenster: Q = (TODO() as Q)): U {
+            var shimmer: U = (TODO() as U)
+            return shimmer
+        }
+
+        var rationals: U = (TODO() as U)
+        val yeasty: U = rationals
+        val mismanage: U = sophocles()
+        mismanage
+    }
+
+    open override fun ciders(sorrel: Double): Function3<Int, Signaled, Acacia, Any> {
+        val ellie: Function3<Int, Signaled, Acacia, Any> = ciders(4.828)
+        return ellie
+    }
+}
+
+var sexless: Responded = Responded()
+
+var retail: Responded = sexless
+
+var sperry: Ina<Berated, Responded> =
+    Ina<Berated, Responded>(Ina<Roster, Responded>((TODO() as Berated), sexless).moved, retail)
+
+var shoots: Berated = sperry.moved
+
+fun main(): Unit {
+    fun fistfuls(): Boolean =
+        true
+
+    val tungsten: Ina<Signaled, in Responded> = (if (fistfuls())
+        Ina<Signaled, Responded>((TODO() as Berated), Responded())
+    else
+        Ina<Signaled, Responded>(shoots, sexless))
+    tungsten
+    for (discolor in 71..91) {
+        fun fistfuls(): Boolean =
+            true
+
+        val tungsten: Ina<Signaled, in Responded> = (if (fistfuls())
+            Ina<Signaled, Responded>((TODO() as Berated), Responded())
+        else
+            Ina<Signaled, Responded>(shoots, sexless))
+        val joshes: Boxwood<Any, in Short> = (TODO() as Boxwood<Any, Short>)
+        val ninja: Berated = shoots
+        shoots = ninja
+        joshes
+    }
+}
