4988c7b7134c364643fb66ea76a6cc9d6532a898
vitekkor <viktor.korotkikh@gmail.com>
2023-06-12 18:02:56 +0300

    #18: update ResultsComaparator.kt

--- testOracle/src/main/kotlin/com/vitekkor/ResultsComaparator.kt
+++ testOracle/src/main/kotlin/com/vitekkor/ResultsComaparator.kt
@@ -3,13 +3,17 @@ package com.vitekkor
 import com.vitekkor.compiler.JavaCompiler
 import com.vitekkor.compiler.KotlinJVMCompiler
 import com.vitekkor.config.CompilerArgs
+import com.vitekkor.perffect.util.BodySurgeon
 import com.vitekkor.project.Language
+import com.vitekkor.project.Project
 import com.vitekkor.project.toProject
 import java.io.File
 import kotlin.system.exitProcess
 
 fun main() {
-    val seed = 459416852774387161
+    val seed = 5335769039710021543
+    val repeatCount: Long = Int.MAX_VALUE.toLong()
+    val withRepeat = true
     val kotlinCompiler = KotlinJVMCompiler()
     val javaCompiler = JavaCompiler()
     kotlinCompiler.cleanUp()
@@ -18,8 +22,16 @@ fun main() {
     val kotlin = File(CompilerArgs.pathToResultsDir + "/$seed", CompilerArgs.pathToTmpDir + "/Main.kt").readText()
     val java = File(CompilerArgs.pathToResultsDir + "/$seed", CompilerArgs.pathToTmpDir + "/Main.java").readText()
 
-    val kotlinProject = kotlin.toProject(Language.KOTLIN)
-    val javaProject = java.toProject(Language.JAVA)
+    val kotlinProject: Project
+    val javaProject: Project
+
+    if (withRepeat) {
+        kotlinProject = BodySurgeon.replaceKotlinMainFun(kotlin, repeatCount).toProject(Language.KOTLIN)
+        javaProject = BodySurgeon.replaceJavaMainFun(java, repeatCount).toProject(Language.JAVA)
+    } else {
+        kotlinProject = kotlin.toProject(Language.KOTLIN)
+        javaProject = java.toProject(Language.JAVA)
+    }
 
     val compiledJava = javaCompiler.tryToCompileWithStatusAndExecutionTime(javaProject)
 
