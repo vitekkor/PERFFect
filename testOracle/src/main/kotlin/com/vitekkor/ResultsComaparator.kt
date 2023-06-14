e7e50eaac63486d7cf0c7bd4bdc9219f3ee98669
vitekkor <viktor.korotkikh@gmail.com>
2023-06-14 09:05:42 +0300

    #18: upgrade ResultsComaparator.kt

--- testOracle/src/main/kotlin/com/vitekkor/ResultsComaparator.kt
+++ testOracle/src/main/kotlin/com/vitekkor/ResultsComaparator.kt
@@ -3,13 +3,17 @@ package com.vitekkor
 import com.vitekkor.compiler.JavaCompiler
 import com.vitekkor.compiler.KotlinJVMCompiler
 import com.vitekkor.config.CompilerArgs
+import com.vitekkor.model.MeasurementResult
+import com.vitekkor.perffect.util.BodySurgeon
 import com.vitekkor.project.Language
 import com.vitekkor.project.toProject
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import java.io.File
 import kotlin.system.exitProcess
 
 fun main() {
-    val seed = 2444720093877148376
+    val seed = -8497514709130667753
     val kotlinCompiler = KotlinJVMCompiler()
     val javaCompiler = JavaCompiler()
     kotlinCompiler.cleanUp()
@@ -18,8 +22,11 @@ fun main() {
     val kotlin = File(CompilerArgs.pathToResultsDir + "/$seed", CompilerArgs.pathToTmpDir + "/Main.kt").readText()
     val java = File(CompilerArgs.pathToResultsDir + "/$seed", CompilerArgs.pathToTmpDir + "/Main.java").readText()
 
-    val kotlinProject = kotlin.toProject(Language.KOTLIN)
-    val javaProject = java.toProject(Language.JAVA)
+    val metaJson = File(CompilerArgs.pathToResultsDir + "/$seed", "meta.json")
+        .let { Json.decodeFromString<MeasurementResult>(it.readText()) }
+
+    val kotlinProject = BodySurgeon.replaceKotlinMainFun(kotlin, metaJson.repeatCount).toProject(Language.KOTLIN)
+    val javaProject = BodySurgeon.replaceJavaMainFun(java, metaJson.repeatCount).toProject(Language.JAVA)
 
     val compiledJava = javaCompiler.tryToCompileWithStatusAndExecutionTime(javaProject)
 
